5d491b4adfdf2b991bc985a18468caaf33c7acdc
本 机 <dbjyz@beier.local>
2015-12-25 17:16:36 +0800

    Initial Commit

---
+++ city/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  city
+//
+//  Created by dbjyz on 15/12/25.
+//  Copyright © 2015年 dbjyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ city/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  city
+//
+//  Created by dbjyz on 15/12/25.
+//  Copyright © 2015年 dbjyz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
